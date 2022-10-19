232f0724ee73613c09d57f4d16c7098e37892972
☁️ <72570835+impulsado@users.noreply.github.com>
2022-10-19 09:34:16 +0200

    (MOD) Modded main.js

--- jss/main.js
+++ jss/main.js
@@ -1,17 +1,17 @@
 const about = document.querySelector('#about')
-const projects = document.querySelector('#contact')
+const projects = document.querySelector('#projects')
 const aboutContent = document.querySelector('#about-content')
 const projectsContent = document.querySelector('#projects-content')
 
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
     title: 'About Me',
-    width: '400px',
-    height: '400px',
+    width: '325px',
+    height: '360px',
     top: 50,
     right: 50,
     bottom: 50,
-    left: 50,
+    left: 30,
     mount: aboutContent,
     onfocus: function () {
       this.setBackground('#5B5B5B')
@@ -25,12 +25,12 @@ about.addEventListener('click', () => {
 projects.addEventListener('click', () => {
   const projects = new WinBox({
     title: 'Projects',
-    width: '400px',
-    height: '400px',
+    width: '325px',
+    height: '360px',
     top: 50,
     right: 50,
     bottom: 50,
-    left: 50,
+    left: 30,
     mount: projectsContent,
     onfocus: function () {
       this.setBackground('#5B5B5B')
